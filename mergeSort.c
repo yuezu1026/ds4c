b4290beb605f8c0213530082e5c756b2cce4171b
yuezu1026 <yuezu1026@sina.com>
2020-07-24 09:48:05 +0800

    update

--- mergeSort.c
+++ mergeSort.c
@@ -45,6 +45,45 @@ void Merge(RecType R[],int low,int mid,int high) //¹é²¢R[low..high];
     free(R1);
 }
 
+void MergePass(RecType R[],int length,int n)
+{
+    int i;
+    for(i=0;i+2*length-1<n;i=i+2*length)
+    {
+        Merge(R,i,i+length-1,i+2*length-1);
+    }
+    if(i+length-1 < n-1)
+    {
+        Merge(R,i,i+length -1,n-1);
+    }
+}
+
+void MergeSort(RecType R[],int n)
+{
+    int length;
+    for(length =1;length<n;length =2 * length)
+    {
+        MergePass(R,length,n);
+    }
+}
+
+void MergeSortDC(RecType R[],int low, int high)
+{
+    int mid;
+    if(low < high)
+    {
+        mid = (low + high) /2;
+        MergeSortDC(R,low,mid);
+        MergeSortDC(R,mid+1,high);
+        Merge(R,low,mid,high);
+    }
+}
+
+void MergeSort1(RecType R[], int n)
+{
+    MergeSortDC(R,0,n-1);
+}
+
 void main()
 {
     printf("helloworld");
